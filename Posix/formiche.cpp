7daa4658a5b27302ec8e296ca26f5faa16547b28
Emanuele Conforti <manu.conforti@gmail.com>
2022-12-30 17:37:30 +0100

    Esercizio formiche MPI/Posix

--- Posix/formiche.cpp
+++ Posix/formiche.cpp
@@ -1,103 +1,152 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <windows.h>
 
-#define SIZE 10
-#define steps 5
-#define numFormiche 2
-
-int celle[SIZE];
-
-
-class Formica {
-public:
-    int pos;
-    int dir;
-    int idForm;
-
-    Formica(int i) {
-        idForm = i;
-        srand(time(0));
-        pos = rand() % SIZE;
-        dir = rand() % 2;
-        celle[pos] = idForm;        
-    }
+struct ant {
+    int pos; 
+    int dir; // 0 ovest, 1 est
+    pthread_t thid;
 };
 
-int idThread = 0;
-pthread_t threads[numFormiche];
-Formica formiche[numFormiche];
+struct cell {
+    pthread_mutex_t m;
+    pthread_cond_t c;
+    ant* ant;
+};
+
+ant* ants;
+cell* cells;
+int nAnts;
+int nCells;
+pthread_cond_t cond;
 pthread_mutex_t mutex;
+bool start = false;
+
 
 void init() {
-    for(int i = 0; i < SIZE; ++i)
-        celle[i] = -1;
-    
-    for(int i = 0; i < numFormiche; ++i)
-        formiche[i] = Formica(i);    
-}
+    cells = new cell[nCells];
 
-
-void printArray() {
-    printf("----- Stampa array -----\n|");
-    for(int i = 0; i < SIZE; ++i) {
-        if(celle[i] != -1)
-            printf("%d|", 1);
-        else printf("%d|", 0);
+    for(int i = 0; i < nCells; i++) {
+        pthread_mutex_init(&cells[i].m, NULL);
+        pthread_cond_init(&cells[i].c, NULL);
+        for(int j = 0; j < nAnts; j++) {
+            if(ants[j].pos == i)
+                cells[i].ant = &ants[j];
+        }
     }
-    printf("\n");
 }
 
-void* move(void* arg) {
-    Formica f =  *(Formica*) arg;
+void move(ant& a) {
 
+    bool locked = false, moved = false;
+
+    if(a.pos > 0 && a.dir == 0) {
+        pthread_mutex_lock(&cells[a.pos-1].m);
+        locked = true;
+        if(cells[a.pos-1].ant == NULL) {
+            pthread_mutex_lock(&cells[a.pos].m);
+            ant* tmp = cells[a.pos].ant;
+            cells[a.pos].ant = NULL;
+            cells[a.pos-1].ant = tmp;
+            pthread_mutex_unlock(&cells[a.pos].m);
+            a.pos--;
+            moved = true;
+            printf("Formica %d con dir %d in nuova pos %d\n", a.thid, a.dir, a.pos);
+            printf("Formica %d unlock01 di pos %d\n", a.thid, a.pos);
+            pthread_mutex_unlock(&cells[a.pos].m); 
+        }
+
+        else {
+            a.dir = 1;
+            printf("Formica %d in pos %d incontra una formica e cambia dir in %d\n", a.thid, a.pos, a.dir);
+            printf("Formica %d unlock0 di pos %d\n", a.thid, a.pos-1);
+            pthread_mutex_unlock(&cells[a.pos-1].m);
+        }
+    }
+
+    else if(a.pos < (nCells-1) && a.dir == 1) {
+        pthread_mutex_lock(&cells[a.pos+1].m);
+        locked = true;
+        if(cells[a.pos+1].ant == NULL) {
+            pthread_mutex_lock(&cells[a.pos].m);
+            ant* tmp = cells[a.pos].ant;
+            cells[a.pos].ant = NULL;
+            cells[a.pos+1].ant = tmp;
+            pthread_mutex_unlock(&cells[a.pos].m);
+            a.pos++;           
+            moved = true;
+            printf("Formica %d con dir %d in nuova pos %d\n", a.thid, a.dir, a.pos);
+            printf("Formica %d unlock01 di pos %d\n", a.thid, a.pos);
+            pthread_mutex_unlock(&cells[a.pos].m); 
+        }
+
+        else {
+            a.dir = 0;
+            printf("Formica %d in pos %d incontra una formica e cambia dir in %d\n", a.thid, a.pos, a.dir);
+            printf("Formica %d unlock1 di pos %d\n", a.thid, a.pos+1);
+            pthread_mutex_unlock(&cells[a.pos+1].m);
+        }
+    }
+
+    else if(a.dir == 0) {
+        a.dir = 1;
+        printf("Formica %d in pos %d incontra un muro e cambia dir in %d\n", a.thid, a.pos, a.dir);
+    }
+
+    else {
+        a.dir = 0;
+        printf("Formica %d in pos %d incontra un muro e cambia dir in %d\n", a.thid, a.pos, a.dir);
+    }
+}
+
+void* antRun(void* arg) {
+    ant* me = (ant*)arg;
     pthread_mutex_lock(&mutex);
 
-    if(f.dir == 0) {
-        if(f.pos == 0 || celle[f.pos - 1] != -1)
-            f.dir = 1;
-        else {
-            celle[f.pos - 1] = f.idForm;
-            celle[f.pos] = -1;
-            f.pos--;
-        }
-    }
-    else {
-        if(f.pos == (SIZE - 1) || celle[f.pos + 1] != -1)
-            f.dir = 0;
-        else {
-            celle[f.pos + 1] = f.idForm;
-            celle[f.pos] = -1;
-            f.pos++;
-        }
+    while(!start) {
+        pthread_cond_wait(&cond, &mutex);
     }
 
-    printArray();
     pthread_mutex_unlock(&mutex);
 
-    return NULL;
+    while(true) {
+        move(*me);
+        Sleep(1000);
+    }
 }
 
-int trovaFormica(int pos) {
-    for(int i = 0; i < numFormiche; ++i)
-        if(formiche[i].pos == pos)
-            return i;
-    return -1;
+void setVariables(int numAnts, int numCells) {
+    nAnts = numAnts;
+    nCells = numCells;
+}
+
+void setPosDir(int index, int p, int d) {
+    ants[index].pos = p;
+    ants[index].dir = d; 
+
+    printf("Formica %d: pos %d --- dir %d\n", ants[index].thid, ants[index].pos, ants[index].dir);
 }
 
 int main(int argc, char* argv[]) {
-    init();
     pthread_mutex_init(&mutex, NULL);
+    pthread_cond_init(&cond, NULL);
+    setVariables(2, 5); // nAnts, nCells
+    ants = new ant[nAnts];
 
-    for(int i = 0; i < steps; ++i)
-        for(int j = 0; j < SIZE; j++)
-            if(celle[j] != -1) {
-                int index = trovaFormica(j);
-                pthread_create(&threads[idThread++], NULL, &move, &formiche[index]);
-            }
+    for(int i = 0; i < nAnts; i++) {
+        pthread_create(&ants[i].thid, NULL, &antRun, &ants[i]);
+        setPosDir(i, i*3, i);
+    }
 
-    for(int i = 0; i < numFormiche; ++i) 
-        pthread_join(threads[i], NULL);
-    
-    pthread_mutex_destroy(&mutex);
+    init();
+
+    start = true;
+
+    pthread_cond_broadcast(&cond);
+
+    for(int i = 0; i < nAnts; i++)
+        pthread_join(ants[i].thid, NULL);
+
+    return 0;
 }
